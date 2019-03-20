022e1172e50faff7c71eaf24a3366e16b87ea065
c0ny1 <1627639645@qq.com>
2019-03-20 22:04:40 +0800

    将项目更新为maven项目

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -1,18 +1,16 @@
-﻿package burp;
+package burp;
 
 import java.awt.Component;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.SwingUtilities;
-
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
